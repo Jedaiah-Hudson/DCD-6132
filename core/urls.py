73216aeda94162ca46a8cf75abd731095a13f15e
Pjones97 <168655932+Pjones97@users.noreply.github.com>
2026-03-03 01:36:29 -0500

    Created Login Auth

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
     path('', views.dashboard, name='dashboard'),
     path('notifications/', views.notifications, name='notifications'),
     path('profile/', views.profile, name='profile'),
+    path('login/', views.login_api, name='login_api'), #added for login api view, url route, endpoint is POST /login/
+    path('login-vis/', views.login_view, name='login_view'),  # page to test login
 ]
--- core/views.py
+++ core/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
 
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.authtoken.models import Token
+from .models import User
+
+
+# Create your views here.
 def dashboard(request):
     template_data = {'title': 'Dashboard'}
     return render(request, 'core/dashboard.html', {'template_data': template_data})
@@ -12,4 +20,39 @@ def profile(request):
     template_data = {'title': 'Profile'}
     return render(request, 'core/profile.html', {'template_data': template_data})
 
-# Create your views here.
+# view to login api endpoints
+@api_view(['POST'])
+def login_api(request):
+    email = request.data.get('email')
+    password = request.data.get('password')
+    
+    #check if email exist in database
+    try:
+        user = User.objects.get(email=email)
+    except User.DoesNotExist:
+        return Response ({
+            "success": False,
+            "message": "Email does not exist."
+        }, status=status.HTTP_404_NOT_FOUND)
+   
+    #check if password is correct
+    if not user.check_password(password):
+        return Response ({
+            "success": False,
+            "message": "Incorrect password. Try again."
+        }, status=status.HTTP_401_UNAUTHORIZED)
+   
+    #create/get token for user
+    token, created = Token.objects.get_or_create(user=user)
+    return Response({
+        "success": True,
+        "message": "Login Successful!",
+        "token": token.key,
+        "user": {
+            "id": user.id,
+            "email": user.email
+        }
+    }, status=status.HTTP_200_OK)
+
+def login_view(request):
+    return render(request, 'core/login.html')
