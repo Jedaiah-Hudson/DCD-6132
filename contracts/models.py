6d947f2f6aae55a34fd455807a1808a1dcb4adc5
Pjones97 <168655932+Pjones97@users.noreply.github.com>
2026-04-06 11:57:20 -0400

    contract model added to admin view

--- contracts/models.py
+++ contracts/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Contract(models.Model):
-    id = models.AutoField(primary_key=True)
+
     source = models.CharField(max_length=255)
     source_mailbox = models.CharField(max_length=255)
     title = models.CharField(max_length=255)
@@ -13,10 +13,11 @@ class Contract(models.Model):
     naics_code = models.CharField(max_length=50, blank=True)
     hyperlink = models.URLField(blank=True)
     partner_name = models.CharField(max_length=255, blank=True)
-    notes = models.TextField(blank=True)
     status = models.CharField(max_length=50, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.title
+    
+    
