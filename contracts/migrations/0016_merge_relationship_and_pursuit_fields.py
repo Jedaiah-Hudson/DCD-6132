198daaf3a63a2825631126e39df06b114545b384
Jedaiah-Hudson <jdhud24@gmail.com>
2026-04-27 01:04:13 -0400

    Add contract migration merge node

---
+++ contracts/migrations/0016_merge_relationship_and_pursuit_fields.py
@@ -0,0 +1,13 @@
+# Generated during merge conflict resolution
+
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("contracts", "0013_usercontractprogress_relationship_label"),
+        ("contracts", "0015_merge_20260426_0000"),
+    ]
+
+    operations = []
